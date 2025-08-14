05ce0bf22c1739729b81e01080cc8f485cca4715
monique <moniqueohana@yahoo.com.br>
2025-08-14 18:36:30 -0300

    pag de perfil

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ import DeletarTema from './components/temas/deletartema/DeletarTema';
 import ListaPostagens from './components/postagens/listapostagens/ListaPostagem';
 import FormPostagem from './components/postagens/formpostagens/FormPostagem';
 import DeletarPostagem from './components/postagens/deletarpostagens/DeletarPostagem';
+import Perfil from './pages/perfil/Perfil';
 
 function App() {
     return (
@@ -33,6 +34,7 @@ function App() {
               <Route path="/cadastrarpostagem" element={<FormPostagem />} />
               <Route path="/editarpostagem/:id" element={<FormPostagem />} />
               <Route path="/deletarpostagem/:id" element={<DeletarPostagem />} />
+              <Route path="/perfil" element={<Perfil />} />
             </Routes>
           </div>
           <Footer />
